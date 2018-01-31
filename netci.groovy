e02466709e8542260eb8d79555411b3aa999d40a
Ravi Eda <raeda@microsoft.com>
2018-01-31 16:57:27 -0600

    Split large jobs

--- netci.groovy
+++ netci.groovy
@@ -3,7 +3,7 @@ import jobs.generation.Utilities
 def project = GithubProject
 def branch = GithubBranchName
 def isPR = true
-def distroList = ['alpine', 'centos', 'debian', 'fedora', 'opensuse', 'ubuntu']
+def distroList = ['alpine', 'centos/6', 'centos/7' 'debian', 'fedora', 'opensuse', 'ubuntu/14', 'ubuntu/16', 'ubuntu/17']
 
 distroList.each { distro ->
     def newJobName = Utilities.getFullJobName(project, "${distro}", isPR)
@@ -11,7 +11,7 @@ distroList.each { distro ->
     def newJob = job(newJobName) {
         steps {
             shell("docker build -t runner --pull -f ./Dockerfile.linux.runner .")
-            shell("docker run -v /var/run/docker.sock:/var/run/docker.sock runner pwsh -NoProfile -File ./build.ps1 -DockerfilePath \"src/${distro}/*\" -CleanupDocker")
+            shell("docker run -v /var/run/docker.sock:/var/run/docker.sock runner pwsh -NoProfile -File ./build.ps1 -DockerfilePath \"src/${distro}*\" -CleanupDocker")
         }
     }
 
