cae792c3a300f2429f315b30f9e1256a4901ff1d
Steve MacLean <sdmaclea.qdt@qualcommdatacenter.com>
2018-03-08 18:02:21 -0500

    Add ubuntu-16.04-cross-arm64 image

--- src/ubuntu/build-scripts/build-rootfs.sh
+++ src/ubuntu/build-scripts/build-rootfs.sh
@@ -7,6 +7,8 @@ set -u
 
 os=$1
 crossToolset=$2
+archArg=${3:-}
+lldb=${4:-}
 
 scriptsVolume="scripts$(date +%s)"
 dockerCrossDepsTag="microsoft/dotnet-buildtools-prereqs:${os}-crossdeps"
@@ -17,14 +19,13 @@ docker run --rm -v $scriptsVolume:/scripts $dockerCrossDepsTag git clone https:/
 rm -rf $PWD/rootfs
 mkdir rootfs
 
-# To support additional architectures for which cross toolset needs to be setup,
-# simply add it to crossArchArray below.
-crossArchArray=("arm" "x86")
+# If argument three was set, use that as the only arch, otherwise use default list of arches : 'arm x86'
+crossArchArray=(${archArg:-'arm x86'})
 for arch in $crossArchArray
 do
     echo "Using $dockerCrossDepsTag to set up cross-toolset for $arch for $crossToolset"
     buildRootFSContainer="rootfs-$arch-$crossToolset"
     docker run --privileged --rm --name $buildRootFSContainer -e ROOTFS_DIR=/rootfs/$arch \
         -v $PWD/rootfs:/rootfs -v $scriptsVolume:/scripts \
-        $dockerCrossDepsTag /scripts/cross/build-rootfs.sh $arch $crossToolset --skipunmount
+        $dockerCrossDepsTag /scripts/cross/build-rootfs.sh $arch $crossToolset $lldb --skipunmount
 done
