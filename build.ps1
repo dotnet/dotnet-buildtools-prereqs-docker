f4740122177360717a755f1b5e3ea53fb1d1d7fe
Davis Goodin <dagood@microsoft.com>
2018-07-24 12:06:44 -0500

    Add rhel 7.5 Dockerfile and infrastructure
    Add "-RunPrivileged" arg and upgrade image-builder.

--- build.ps1
+++ build.ps1
@@ -2,7 +2,12 @@
 param(
     [string]$DockerfilePath = "*",
     [string]$ImageBuilderCustomArgs,
-    [switch]$CleanupDocker
+    [switch]$CleanupDocker,
+
+    # Adds "--privileged" to the "docker run" command. In unknown circumstances we encounter on the
+    # RHEL build agent, the container must be run with --privileged in order to access the shared
+    # docker socket. See https://stackoverflow.com/a/36614457 for an untried potential alternative.
+    [switch]$RunPrivileged
 )
 
 Set-StrictMode -Version Latest
@@ -23,7 +28,13 @@ try {
         throw "Failed building ImageBuilder."
     }
 
+    $runPrivilegedArg = ""
+    if ($RunPrivileged) {
+        $runPrivilegedArg = "--privileged "
+    }
+
     $expression = "docker run --rm " +
+        $runPrivilegedArg +
         "-v /var/run/docker.sock:/var/run/docker.sock " +
         "imagebuilder " +
         "build --manifest manifest.json --path '$DockerfilePath' $ImageBuilderCustomArgs"
