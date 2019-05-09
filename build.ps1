3945ab5775e964ac511a2da7010c7bee3ad28ea0
Michael Simons <msimons@microsoft.com>
2019-05-09 10:17:33 -0500

    Add support for building Windows based Dockerfiles (#138)

--- build.ps1
+++ build.ps1
@@ -1,40 +1,15 @@
 [cmdletbinding()]
 param(
     [string]$DockerfilePath = "*",
-    [string]$ImageBuilderCustomArgs,
-    [string]$RunCustomArgs,
-    [switch]$CleanupDocker
+    [string]$ImageBuilderCustomArgs
 )
 
 Set-StrictMode -Version Latest
 $ErrorActionPreference = 'Stop'
-
-function Invoke-CleanupDocker()
-{
-    if ($CleanupDocker) {
-        docker system prune -a -f
-    }
-}
-
-Invoke-CleanupDocker
-
+pushd $PSScriptRoot
 try {
-    & docker build -t imagebuilder --pull -f ./Dockerfile.linux.imagebuilder .
-    if ($LastExitCode -ne 0) {
-        throw "Failed building ImageBuilder."
-    }
-
-    $expression = "docker run --rm " +
-        "-v /var/run/docker.sock:/var/run/docker.sock " +
-        "$RunCustomArgs imagebuilder " +
-        "build --manifest manifest.json --path '$DockerfilePath' $ImageBuilderCustomArgs"
-
-    Invoke-Expression $expression
-
-    if ($LastExitCode -ne 0) {
-        throw "Failed executing ImageBuilder."
-    }
+    ./scripts/Invoke-ImageBuilder.ps1 "build --path '$DockerfilePath' $ImageBuilderCustomArgs"
 }
 finally {
-    Invoke-CleanupDocker
+    popd
 }
