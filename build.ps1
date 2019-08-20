77d79da2f53a2942f13af1e52a43df654772a107
Matt Thalman <mthalman@microsoft.com>
2019-08-20 12:50:36 -0500

    Fix build script reference

--- build.ps1
+++ build.ps1
@@ -8,7 +8,7 @@ Set-StrictMode -Version Latest
 $ErrorActionPreference = 'Stop'
 pushd $PSScriptRoot
 try {
-    ./scripts/Invoke-ImageBuilder.ps1 "build --path '$DockerfilePath' $ImageBuilderCustomArgs"
+    ./eng/common/Invoke-ImageBuilder.ps1 "build --path '$DockerfilePath' $ImageBuilderCustomArgs"
 }
 finally {
     popd
