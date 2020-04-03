3d73ec85b80848ef21b3b11d861a1e834cba41f0
Tomas Weinfurt <tweinfurt@yahoo.com>
2020-04-03 08:54:35 -0700

    add images to build runtime on FreeBSD (#277)

--- src/ubuntu/build-scripts/build-rootfs.sh
+++ src/ubuntu/build-scripts/build-rootfs.sh
@@ -32,7 +32,7 @@ do
 
     echo "Using $dockerCrossDepsTag to clone arcade to fetch scripts used to build cross-toolset"
     docker exec $buildRootFSContainer \
-        git clone https://github.com/dotnet/arcade /scripts
+        git clone --depth 1 https://github.com/dotnet/arcade /scripts
 
     if [ $? -ne 0 ]; then
         echo "Rootfs build failed: `git clone https://github.com/dotnet/arcade /scripts` returned error"
