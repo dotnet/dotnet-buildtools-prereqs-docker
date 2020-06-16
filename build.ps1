482e4e90ed123c30950e84c3442501bda5141109
Matt Thalman <mthalman@microsoft.com>
2020-06-16 10:14:36 -0500

    Set UniqueId variable in build script

--- build.ps1
+++ build.ps1
@@ -7,6 +7,7 @@ param(
 Set-StrictMode -Version Latest
 $ErrorActionPreference = 'Stop'
 pushd $PSScriptRoot
+$ImageBuilderCustomArgs = "$ImageBuilderCustomArgs --var UniqueId=$(Get-Date -Format yyyyMMddHHmmss)"
 try {
     ./eng/common/Invoke-ImageBuilder.ps1 "build --path '$DockerfilePath' $ImageBuilderCustomArgs"
 }
