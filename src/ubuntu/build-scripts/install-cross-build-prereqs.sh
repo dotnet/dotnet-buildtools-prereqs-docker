5db2efc767a4508b38f56229eeeea5d9afd1e5a2
Adeel <3840695+am11@users.noreply.github.com>
2022-08-01 23:24:05 +0300

    Use Ubuntu 20.04 (or better) repositories on host

--- src/ubuntu/build-scripts/install-cross-build-prereqs.sh
+++ src/ubuntu/build-scripts/install-cross-build-prereqs.sh
@@ -5,6 +5,10 @@ set -e
 # Stop script if unbound variable found (use ${var:-} if intentional)
 set -u
 
+# we need to make sure that we install binfmt >= 2.2.0 and qemu-user-static >= 1.4.2,
+# which are available in Ubuntu 20.04+ repositories
+sudo sh -c '(. /etc/os-release && [ "$(echo "$VERSION_ID" | tr -d .)" -lt 2004 ] && sed -i "s/$UBUNTU_CODENAME/focal/g" /etc/apt/sources.list)'
+
 # see (see https://github.com/dotnet/dotnet-buildtools-prereqs-docker/issues/120)
 sudo apt-get update
 sudo apt-get install -y \
