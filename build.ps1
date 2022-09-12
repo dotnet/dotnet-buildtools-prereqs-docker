7f4fe3005ebf4d6eb7d1a9f8a468c828985bcaa5
Michelle McDaniel <adiaaida@gmail.com>
2022-09-12 10:09:13 -0700

    Add latest/staging tag to all docker images (#667)

--- build.ps1
+++ build.ps1
@@ -7,7 +7,7 @@ param(
 Set-StrictMode -Version Latest
 $ErrorActionPreference = 'Stop'
 pushd $PSScriptRoot
-$ImageBuilderCustomArgs = "$ImageBuilderCustomArgs --var UniqueId=$(Get-Date -Format yyyyMMddHHmmss)"
+$ImageBuilderCustomArgs = "$ImageBuilderCustomArgs --var UniqueId=$(Get-Date -Format yyyyMMddHHmmss) --var FloatingTagSuffix=local"
 try {
     ./eng/common/Invoke-ImageBuilder.ps1 "build --path '$DockerfilePath' $ImageBuilderCustomArgs"
 }
