e09d81ea73ff103f293f825521253598a3ab6cbb
Michael Simons <msimons@microsoft.com>
2024-08-15 16:27:28 -0500

    Remove timestamp tags and update readme (#1178)
    Co-authored-by: Matt Thalman <mthalman@microsoft.com>

--- build.ps1
+++ build.ps1
@@ -7,7 +7,6 @@ param(
 Set-StrictMode -Version Latest
 $ErrorActionPreference = 'Stop'
 pushd $PSScriptRoot
-$ImageBuilderCustomArgs = "$ImageBuilderCustomArgs --var UniqueId=$(Get-Date -Format yyyyMMddHHmmss)"
 try {
     ./eng/common/Invoke-ImageBuilder.ps1 "build --path '$DockerfilePath' $ImageBuilderCustomArgs"
 }
