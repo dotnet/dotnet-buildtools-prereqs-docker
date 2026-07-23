7a0d41e069580753d4d97973855f65acc61b8558
Premek Vysoky <premek.vysoky@microsoft.com>
2026-07-23 17:05:50 +0200

    Simplify `dotnet run`

--- run-tests.ps1
+++ run-tests.ps1
@@ -23,14 +23,9 @@ function Exec {
     }
 }
 
-$EngDockerToolsDir = "$PSScriptRoot/eng/docker-tools"
-
-$DotnetInstallDir = "$PSScriptRoot/.dotnet"
-& $EngDockerToolsDir/Install-DotNetSdk.ps1 -InstallPath $DotnetInstallDir
-
 Push-Location "$PSScriptRoot\tests\Microsoft.DotNet.BuildTools.Prereqs.Docker.Tests"
 try {
-    Exec "$DotnetInstallDir/dotnet run --project Microsoft.DotNet.BuildTools.Prereqs.Docker.Tests.csproj --report-xunit-trx --results-directory $PSScriptRoot/artifacts/TestResults $OptionalArgs"
+    Exec "$PSScriptRoot/eng/common/dotnet.ps1 run --project Microsoft.DotNet.BuildTools.Prereqs.Docker.Tests.csproj --report-xunit-trx --results-directory $PSScriptRoot/artifacts/TestResults $OptionalArgs"
 }
 finally {
     Pop-Location
